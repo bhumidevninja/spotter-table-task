f6672b8b4e7951e3b432c640d65c3efe935d04e7
bhumidevninja <bhumidevninja@gmail.com>
2024-08-22 18:25:39 +0530

    Added devDependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import DataTableView from "./components/DataTableView";
 import Navbar from "./components/Navbar";
-import {PivoteTable}  from "./components/PivoteTable";
+import { PivoteTable } from "./components/PivoteTable";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
